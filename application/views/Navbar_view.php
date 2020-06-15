8deba3baac9cc29812818745c2920aeb36c11701
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-15 15:44:48 +0200

    * Configuration de CodeIgniter en français

--- application/views/Navbar_view.php
+++ application/views/Navbar_view.php
@@ -1,3 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top scrolling-navbar">
-    test
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#">
+            <img src="https://mdbootstrap.com/img/logo/mdb-transparent.png" height="30" alt="Logo Mémoire des Lieux">
+        </a>
+        <ul class="navbar-nav mr-auto mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+    </div>
 </nav>
