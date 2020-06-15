630d2c4e5ebe7be9d52d3dad4d5d2b5a09cb954c
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-15 11:01:58 +0200

    * Ajout des vues Footer_view, Head_view et Navbar_view

---
+++ application/views/Head_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Mémoire des Lieux</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="<?php echo base_url()?>assets/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet">
+</head>
