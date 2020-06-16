35e83b5069a0711361a7a67c12082850eb55aad4
Simonbtl <butelsimon@orange.fr>
2020-06-16 11:00:36 +0200

    *Ajout page d'acceuil

--- application/views/Accueil_view.php
+++ application/views/Accueil_view.php
@@ -1,30 +1,308 @@
 <?php $this->load->view('Head_view'); ?>
 
-<body class="">
+<body class="orange lighten-5">
 <?php $this->load->view('Navbar_view'); ?>
-<div class="container">
-    <p class="py-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis nunc tellus. Fusce auctor lorem eget vehicula semper. Quisque vitae congue est, vel accumsan velit. Donec eu imperdiet augue, eget lobortis odio. Nam vel sem non velit imperdiet porta nec pharetra mauris. Nunc imperdiet mauris lectus, eu pharetra erat cursus sed. Fusce neque nunc, scelerisque eget enim ut, bibendum interdum sem.
+<!--Main Navigation-->
 
-Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse augue justo, ultricies et diam vel, mollis dignissim magna. Pellentesque sed tincidunt erat, at sollicitudin sapien. Praesent in fermentum velit, non rutrum erat. Integer auctor nec nunc ac pretium. Vivamus at libero id tortor mollis suscipit a sit amet enim. Pellentesque sit amet malesuada lacus. Donec quam lectus, ornare consectetur suscipit sit amet, ullamcorper at turpis. Suspendisse nulla felis, accumsan at urna at, suscipit porttitor quam. Ut posuere at massa eu blandit. Donec nisl tortor, viverra sit amet augue eget, aliquam congue lorem. Ut nec posuere massa. Maecenas mollis diam at lacus vulputate, at volutpat tellus iaculis.
+<!--Mask-->
+<div id="intro" class="view">
 
-Cras viverra tempus malesuada. Nam vitae varius tortor, sed placerat sem. Sed vel placerat nisi, sed tincidunt quam. Etiam finibus accumsan suscipit. Cras at ex at elit tincidunt luctus. Nunc feugiat vel enim ac malesuada. Aliquam erat volutpat. In hac habitasse platea dictumst. Phasellus pharetra finibus rutrum. Suspendisse ac lorem ut felis gravida consequat. Phasellus in tincidunt nulla. Maecenas vel mollis arcu. Cras aliquam erat tempus, suscipit augue sed, dignissim tellus. Donec lobortis arcu in purus venenatis posuere id et orci. Duis vehicula ex sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    <div class="mask rgba-black-strong">
 
-Nunc nisi diam, vehicula vel enim vitae, varius euismod mi. Etiam ornare ultricies interdum. Praesent vestibulum rhoncus malesuada. Curabitur enim quam, mollis non metus id, iaculis fringilla orci. Donec eget vehicula sem. Donec a pharetra ante, at consectetur metus. Praesent ornare efficitur enim, at accumsan libero bibendum ac. Nam vel nunc ultrices, egestas nisl at, vestibulum velit. Aenean id metus eu neque pretium vestibulum ac quis quam. Aenean commodo sit amet nisi nec dapibus.
+        <div class="container-fluid d-flex align-items-center justify-content-center h-100">
 
-Morbi consequat leo sed nisi sollicitudin, nec lacinia mauris iaculis. Quisque dictum euismod ligula interdum consectetur. Integer maximus ut magna ac lobortis. Sed hendrerit leo sapien, a venenatis quam varius a. Aenean porta molestie nulla. Integer eget iaculis magna. Maecenas lobortis varius mi eu condimentum. Integer pellentesque justo et lorem convallis cursus. Maecenas vel turpis metus. Maecenas fermentum libero ut tempor euismod.
+            <div class="row d-flex justify-content-center text-center">
 
-Nulla consectetur justo dui, eget interdum arcu imperdiet sed. Pellentesque at iaculis est, et tincidunt purus. Nunc hendrerit ultrices cursus. Phasellus dignissim condimentum turpis condimentum vestibulum. Etiam ac luctus nisi, a tincidunt tellus. Integer maximus, nunc vitae consequat auctor, dolor velit finibus ligula, eu consectetur odio ligula nec lacus. Donec sagittis dui eget ipsum condimentum commodo.
+                <div class="col-md-10">
 
-Praesent in faucibus ante, nec rhoncus enim. Maecenas consectetur, lacus id pulvinar hendrerit, sapien magna eleifend ipsum, eget finibus quam metus et sapien. Donec consequat dolor ut elit volutpat maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras sed dolor felis. Sed neque purus, fringilla vitae laoreet quis, eleifend ut nulla. Ut eget erat dui. Sed mollis felis in turpis ultrices, id hendrerit lectus cursus. Cras sed finibus eros. Maecenas luctus sit amet massa a dignissim. Etiam volutpat felis quis lacus placerat, nec eleifend magna pulvinar. Maecenas arcu nibh, pellentesque in quam eu, pharetra ornare leo. Sed pulvinar, sapien non bibendum vulputate, orci ligula finibus enim, a aliquam tortor elit id nibh. Vestibulum ut lorem neque. Donec tempor varius auctor.
+                    <!-- Heading -->
+                    <img class="w-50 p-3 center-block" src="assets/images/logo_memoire-des-lieux.png">
 
-Pellentesque vitae pretium augue, sit amet facilisis ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer fringilla arcu ex. Nunc cursus orci vitae mauris fringilla facilisis id vitae ipsum. Aenean scelerisque massa sit amet ipsum ornare tristique. Fusce a viverra erat. Vestibulum tristique molestie pulvinar. Fusce aliquam, ligula non egestas pretium, odio felis blandit dui, quis congue purus nibh id est. Nulla dictum est lacinia mi commodo interdum. Suspendisse et laoreet ante. Sed ut egestas quam, eu varius dolor. Cras velit leo, pulvinar at rutrum eu, elementum a orci. Aenean tempus condimentum massa a aliquam. Fusce aliquam fermentum justo ut pulvinar. Praesent vitae condimentum tellus.
+                    <!-- Divider -->
+                    <hr class="hr-light">
 
-Cras consectetur nulla tempor libero tincidunt, ut feugiat tellus gravida. Sed eget euismod ante. Aliquam metus justo, blandit id aliquam sed, elementum vel libero. Donec vitae pellentesque ipsum. Nam gravida augue a mi auctor tincidunt. Nulla rhoncus elit ut lectus porta, vitae egestas nisi cursus. Etiam molestie ligula eu egestas interdum. Fusce gravida consequat nibh a porttitor. Sed non urna ultricies, volutpat purus eget, dignissim nulla. Maecenas at odio eu magna finibus sodales quis a lectus. Integer mauris turpis, consequat quis commodo non, luctus tristique urna. Morbi ut nunc dictum, consequat lacus at, malesuada dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat aliquam dui pulvinar aliquet. Pellentesque sollicitudin sapien odio, vel suscipit lorem euismod id. Nunc nec elementum mauris.
+                    <!-- Description -->
+                    <h4 class="white-text my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Deleniti
+                        consequuntur.</h4>
+                    <a href="#1" class="btn-scroll scrollto" title="Scroll Down"><i class="fas fa-angle-down"></i></a>
 
-Morbi eu semper enim. Sed a aliquet nulla. Nam feugiat, nisl ac aliquam convallis, est sapien mollis leo, at pharetra arcu erat et purus. Vestibulum malesuada varius dui at interdum. Vivamus eu vehicula eros. Maecenas vitae gravida velit, ut dictum risus. Ut viverra efficitur ante id pellentesque.
-    </p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </div>
+<!--/.Mask-->
+<!--Main layout-->
+<main class="mt-5">
+    <div class="container">
+
+        <!--Section: Best Features-->
+        <section id="best-features" class="text-center">
+
+            <!-- Heading -->
+            <h2 class="mb-5 font-weight-bold">Lorem ipsum</h2>
+
+            <!--Grid row-->
+            <div class="row d-flex justify-content-center mb-4">
+
+                <!--Grid column-->
+                <div class="col-md-8">
+
+                    <!-- Description -->
+                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi voluptate
+                        hic
+                        provident nulla repellat
+                        facere esse molestiae ipsa labore porro minima quam quaerat rem, natus repudiandae debitis
+                        est
+                        sit pariatur.</p>
+
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-4 mb-5">
+                    <i class="fa fa-lightbulb fa-4x brown-text lighten-2"></i>
+                    <h4 class="my-4 font-weight-bold">Title</h4>
+                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
+                        maiores nam,
+                        aperiam minima
+                        assumenda deleniti hic.</p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-4 mb-1">
+                    <i class="fa fa-globe fa-4x brown-text lighten-2"></i>
+                    <h4 class="my-4 font-weight-bold">Make research</h4>
+                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
+                        maiores nam,
+                        aperiam minima
+                        assumenda deleniti hic.</p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-4 mb-1">
+                    <i class="fa fa-cog fa-4x brown-text lighten-2"></i>
+                    <h4 class="my-4 font-weight-bold">Do the work</h4>
+                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
+                        maiores nam,
+                        aperiam minima
+                        assumenda deleniti hic.</p>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+        </section>
+        <!--Section: Best Features-->
+
+        <hr class="my-5">
+
+        <!--Section: Examples-->
+        <section id="examples" class="text-center">
+
+            <!-- Heading -->
+            <h2 class="mb-5 font-weight-bold">Carte intéractive</h2>
+            <h3>Rennes</h3>
+
+            <!-- Card -->
+            <div class="card card-cascade wider reverse">
+
+                <!-- Card image -->
+                <div class="view view-cascade overlay">
+                    <img class="card-img-top" src="assets/images/carte-interactive.png"
+                         alt="Card image cap">
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!-- Card content -->
+
+
+                <!-- Title -->
+                <button type="button" class="btn brown lighten-2 card-body card-body-cascade text-center white-text">S'y rendre</button>
+
+
+            </div>
+            <!-- Card -->
+
+            <p class="mt-5">Lorem ipsum Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+
+        </section>
+        <!--Section: Examples-->
+
+        <hr class="my-5">
+
+        <!--Section: Gallery-->
+        <section id="gallery">
+
+            <!-- Heading -->
+            <h2 class="mb-5 font-weight-bold text-center">Découvrez les dernières modifications</h2>
+            <p class="text-center">Lorem ipsumdszqiudjhbsiqjfbnsijbhfnijdshnfoijdshnfdoijkpfhndsoijfds</p>
+
+            <div id="carousel-example-multi" class="carousel slide carousel-multi-item v-2" data-ride="carousel">
+
+                <!--Controls-->
+                <div class="controls-top">
+                    <a class="btn-floating brown lighten-2" href="#carousel-example-multi" data-slide="prev"><i
+                                class="fas fa-chevron-left"></i></a>
+                    <a class="btn-floating brown lighten-2" href="#carousel-example-multi" data-slide="next"><i
+                                class="fas fa-chevron-right"></i></a>
+                </div>
+                <!--/.Controls-->
+
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-multi" data-slide-to="0" class="brown lighten-2 active"></li>
+                    <li class="brown lighten-2" data-target="#carousel-example-multi" data-slide-to="1"></li>
+                    <li class="brown lighten-2" data-target="#carousel-example-multi" data-slide-to="2"></li>
+                </ol>
+                <!--/.Indicators-->
+
+                <div class="carousel-inner v-2" role="listbox">
+
+                    <div class="carousel-item active">
+                        <div class="col-12 col-md-4">
+                            <div class="card mb-2">
+                                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/img (36).jpg"
+                                     alt="Card image cap">
+                                <div class="card-body brown lighten-2">
+                                    <h4 class="card-title font-weight-bold text-center">Name</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="col-12 col-md-4">
+                            <div class="card mb-2">
+                                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/img (36).jpg"
+                                     alt="Card image cap">
+                                <div class="card-body brown lighten-2">
+                                    <h4 class="card-title font-weight-bold text-center">Name</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="col-12 col-md-4">
+                            <div class="card mb-2">
+                                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/img (36).jpg"
+                                     alt="Card image cap">
+                                <div class="card-body brown lighten-2">
+                                    <h4 class="card-title font-weight-bold text-center">Name</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--Section: Gallery-->
+
+        <hr class="my-5">
+
+        <!--Section: Contact-->
+        <section id="contact">
+
+            <!-- Heading -->
+            <h2 class="mb-5 font-weight-bold text-center">Contact us</h2>
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-lg-5 col-md-12">
+                    <!-- Form contact -->
+                    <form class="p-5 grey-text">
+                        <div class="md-form form-sm brown-text lighten-2"> <i class="fa fa-user prefix"></i>
+                            <input type="text" id="form3" class="form-control form-control-sm">
+                            <label for="form3">Nom</label>
+                        </div>
+                        <div class="md-form form-sm brown-text lighten-2"> <i class="fa fa-envelope prefix"></i>
+                            <input type="text" id="form2" class="form-control form-control-sm">
+                            <label for="form2">E-mail</label>
+                        </div>
+                        <div class="md-form form-sm brown-text lighten-2"> <i class="fa fa-tag prefix"></i>
+                            <input type="text" id="form32" class="form-control form-control-sm">
+                            <label for="form34">Sujet</label>
+                        </div>
+                        <div class="md-form form-sm brown-text lighten-2"> <i class="fa fa-pencil prefix"></i>
+                            <textarea type="text" id="form8" class="md-textarea form-control form-control-sm" rows="4"></textarea>
+                            <label for="form8">Votre message</label>
+                        </div>
+                        <div class="text-center mt-4">
+                            <button class="btn btn-primary brown lighten-2">Envoyer<i class="fa fa-paper-plane-o ml-1"></i></button>
+                        </div>
+                    </form>
+                    <!-- Form contact -->
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-7 col-md-12">
+
+                    <!--Grid row-->
+                    <div class="row text-center">
+
+                        <!--Grid column-->
+                        <div class="col-lg-4 col-md-12 mb-3">
+
+                            <p><i class="fa fa-map fa-1x mr-2 brown-text lighten-2"></i>Troyes, 10000</p>
+
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-lg-4 col-md-6 mb-3">
+
+                            <p><i class="fa fa-building fa-1x mr-2 brown-text lighten-2"></i>Lundi - Samedi, 8:00-22:00</p>
+
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-lg-4 col-md-6 mb-3">
+
+                            <p><i class="fa fa-phone fa-1x mr-2 brown-text lighten-2"></i>+ 03 05 16 46 58</p>
+
+                        </div>
+                        <!--Grid column-->
+
+                    </div>
+                    <!--Grid row-->
+
+                    <!--Google map-->
+                    <div id="map-container" class="z-depth-1-half map-container mb-5" style="height: 400px"></div>
+
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+        </section>
+        <!--Section: Contact-->
+
+    </div>
+</main>
+<!--Main layout-->
 <?php $this->load->view('Footer_view'); ?>
 
 </body>
