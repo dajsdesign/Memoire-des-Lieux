61565524e99352ffbe123e4a74ed2186705c76d8
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-16 10:56:23 +0200

    * Navbar_view.php modifié * style.css modifié

--- application/views/Navbar_view.php
+++ application/views/Navbar_view.php
@@ -3,7 +3,7 @@
             aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse w-100 scrolling-navbar" id="navbar">
+    <div class="collapse navbar-collapse w-100 mx-5 scrolling-navbar" id="navbar">
         <!-- Logo -->
         <a class="navbar-brand" href="<?php echo base_url() ?>">
             <img src="<?php echo base_url() ?>assets/images/logo_memoire-des-lieux.png" height="150" alt="Logo Mémoire des Lieux">
@@ -17,14 +17,14 @@
 
 
         <!-- Liens -->
-        <ul class="navbar-nav ml-auto mt-lg-0">
+        <ul class="navbar-nav ml-auto mt-0">
 
             <!-- Dropdown -->
             <li class="nav-item dropdown text-center px-3">
                 <a class="btn-floating waves-effect btn-md cloudy-knoxville-gradient dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="black-text far fa-eye fa-lg"></i>
+                    <i class="marrondylan far fa-eye fa-lg"></i>
                 </a>
-                <div class="dropdown-menu dropdown-primary" aria-labelledby="dropdown">
+                <div class="dropdown-menu dropdown-dark" aria-labelledby="dropdown">
                     <a class="dropdown-item" href="#">Quartiers</a>
                     <a class="dropdown-item" href="#">Rues</a>
                     <a class="dropdown-item" href="#">Parcelles</a>
@@ -41,7 +41,7 @@
             <!-- Recherche avancée -->
             <li class="nav-item text-center px-3">
                 <a class="btn-floating waves-effect  btn-md cloudy-knoxville-gradient" href="#">
-                    <i class="black-text fas fa-search fa-lg"></i>
+                    <i class="marrondylan fas fa-search fa-lg"></i>
                 </a>
                 <p>
                     Recherche avancée
@@ -51,7 +51,7 @@
             <!-- Aide & Contact -->
             <li class="nav-item text-center px-3">
                 <a class="btn-floating waves-effect btn-md cloudy-knoxville-gradient" href="#">
-                    <i class="black-text far fa-life-ring fa-lg"></i>
+                    <i class="marrondylan far fa-life-ring fa-lg"></i>
                 </a>
                 <p>
                     Aide et contact
@@ -61,10 +61,27 @@
             <!-- Connexion -->
             <li class="nav-item text-center px-3">
                 <a class="btn-floating waves-effect btn-md cloudy-knoxville-gradient" href="#">
-                    <i class="black-text fas fa-user fa-lg"></i>
+                    <i class="marrondylan fas fa-user fa-lg"></i>
                 </a>
                 <p>
                     Connexion
+                </p>
+            </li>
+
+            <!-- Langue -->
+            <li class="nav-item dropdown text-center px-3">
+                <a class="btn-floating waves-effect btn-md cloudy-knoxville-gradient dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="marrondylan fas fa-globe-europe fa-lg"></i>
+                </a>
+                <div class="dropdown-menu dropdown-dark" aria-labelledby="dropdown">
+                    <a class="dropdown-item" href="#">Français</a>
+                    <a class="dropdown-item" href="#">Anglais</a>
+                    <a class="dropdown-item text-center" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="black-text far fa-times-circle fa-lg"></i>
+                    </a>
+                </div>
+                <p>
+                    Langue &#9776;
                 </p>
             </li>
         </ul>
