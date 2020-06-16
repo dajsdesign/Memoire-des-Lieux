b2552bb86c0356e1ab4f3611ee6f09eb8ef0185f
romain <romain2109@live.fr>
2020-06-16 14:10:58 +0200

    modification de la position de la carte interactive et du contenue

--- application/views/Consultation_view.php
+++ application/views/Consultation_view.php
@@ -21,41 +21,34 @@
     </div>
 
 </div>
-<!--/.Mask-->
-<!--Main layout-->
 <main class="pt-5 orange lighten-5">
-    <div class="container">
+    <div class="container" style="background-color: white;">
 
-        <!--Section: Best Features-->
-        <section id="best-features" class="text-center">
+        <section id="best-features">
 
-            <!-- Heading -->
-            <h2 class="mb-5 font-weight-bold">Lorem ipsum</h2>
+            <h2 class="mb-5 font-weight-bold">Rennes en carte </h2>
 
-            <!--Grid row-->
-            <div class="row d-flex justify-content-center mb-4">
+            <div class="row d-flex justify-content mb-4">
 
-                <!--Grid column-->
                 <div class="col-md-8">
 
-                    <!-- Description -->
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi voluptate
-                        hic
-                        provident nulla repellat
-                        facere esse molestiae ipsa labore porro minima quam quaerat rem, natus repudiandae debitis
-                        est
-                        sit pariatur.</p>
+                    <p class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod lorem ipsum
+                        dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod Lorem ipsum dolor sit
+                        amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod </p>
 
                 </div>
-                <!--Grid column-->
 
             </div>
-            <!--Grid row-->
+            <section id="examples" class="text-center">
 
-            <!--Grid row-->
-            <div class="row">
+                <h2 class="mb-5 font-weight-bold">Découvrez les dernières modifications
+                </h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
 
-                <!--Grid column-->
+                <section/>
+
+                <!-- <div class="row">
+
                 <div class="col-md-4 mb-5">
                     <i class="fa fa-lightbulb fa-4x brown-text lighten-2"></i>
                     <h4 class="my-4 font-weight-bold">Title</h4>
@@ -64,9 +57,6 @@
                         aperiam minima
                         assumenda deleniti hic.</p>
                 </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
                 <div class="col-md-4 mb-1">
                     <i class="fa fa-globe fa-4x brown-text lighten-2"></i>
                     <h4 class="my-4 font-weight-bold">Make research</h4>
@@ -75,9 +65,6 @@
                         aperiam minima
                         assumenda deleniti hic.</p>
                 </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
                 <div class="col-md-4 mb-1">
                     <i class="fa fa-cog fa-4x brown-text lighten-2"></i>
                     <h4 class="my-4 font-weight-bold">Do the work</h4>
@@ -86,57 +73,26 @@
                         aperiam minima
                         assumenda deleniti hic.</p>
                 </div>
-                <!--Grid column-->
 
-            </div>
-            <!--Grid row-->
+            </div> -->
 
         </section>
-        <!--Section: Best Features-->
 
-        <hr class="my-5">
-
-        <!--Section: Examples-->
         <section id="examples" class="text-center">
 
-            <!-- Heading -->
-            <h2 class="mb-5 font-weight-bold">Carte intéractive</h2>
-            <h3>Rennes</h3>
-
-            <!-- Card -->
             <div class="card card-cascade wider reverse">
 
-                <!-- Card image -->
-                <div class="view view-cascade overlay">
-                    <img class="card-img-top" src="assets/images/carte-interactive.png"
+                <div >
+                    <img class="card-img-top" style="width: 70%" src="assets/images/carte-interactive.png"
                          alt="Card image cap">
                     <a href="#!">
                         <div class="mask rgba-white-slight"></div>
                     </a>
                 </div>
 
-                <!-- Card content -->
-
-
-                <!-- Title -->
-                <button type="button" class="btn brown lighten-2 card-body card-body-cascade text-center white-text">S'y
-                    rendre
-                </button>
-
-
             </div>
-            <!-- Card -->
-
-            <p class="mt-5">Lorem ipsum Le Lorem Ipsum est simplement du faux texte employé dans la composition et la
-                mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les
-                années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre
-                spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la
-                bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années
-                1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment,
-                par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
 
         </section>
-        <!--Section: Examples-->
 
         <hr class="my-5">
 
