a634c86ef2cea6a58bd05b0045fffb541cb3659f
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-17 13:38:58 +0200

    * Prise en charge (très partielle) de la gestion * Ajout de Gestion_Admin_view.php * Affichage du type de compte sur Gestion_view.php et Gestion_Admin_view.php

--- application/controllers/Gestion.php
+++ application/controllers/Gestion.php
@@ -8,20 +8,27 @@ class Gestion extends CI_Controller
         $this->load->model('Head_model');
         $data['toutes'] = $this->Head_model->chargement_head();
         $this->load->view('Head_view', $data);
+        redirect(base_url());
     }
 
     public function user()
     {
+        $this->load->model('Head_model');
+        $data['toutes'] = $this->Head_model->chargement_head();
         $this->load->view('Gestion_User_view');
     }
 
     public function contributor()
     {
+        $this->load->model('Head_model');
+        $data['toutes'] = $this->Head_model->chargement_head();
         $this->load->view('Gestion_Contributor_view');
     }
 
     public function admin()
     {
+        $this->load->model('Head_model');
+        $data['toutes'] = $this->Head_model->chargement_head();
         $this->load->view('Gestion_Admin_view');
     }
 
--- application/views/Head_view.php
+++ application/views/Head_view.php
@@ -5,9 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <?php foreach ($toutes as $ligne ) { ?>
-        <title><?php echo $ligne->site_tabtitle ?></title>
-    <?php } ?>
+    <title>Mémoire des Lieux</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <!-- Bootstrap core CSS -->
