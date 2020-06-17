646dde1fd22c37a24d414417e05bfa0b8169ae8d
Simonbtl <butelsimon@orange.fr>
2020-06-17 10:25:27 +0200

    Ajout de Search_view.php

---
+++ application/views/Search_view.php
@@ -0,0 +1,40 @@
+<div class="modal fade zindex-modal" id="modalSearch" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold brown-text lighten-2">Recherche avancée</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <!-- Search form -->
+            <form class="form-inline mr-auto">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn  btn-rounded btn-sm my-0 brown-text lighten-2" type="submit">Rechercher</button>
+            </form>
+            <div>
+            <!-- Default inline 1-->
+            <div class="custom-control custom-checkbox custom-control-inline">
+                <input type="checkbox" class="custom-control-input" id="defaultInline1">
+                <label class="custom-control-label" for="defaultInline1">1</label>
+            </div>
+
+            <!-- Default inline 2-->
+            <div class="custom-control custom-checkbox custom-control-inline">
+                <input type="checkbox" class="custom-control-input" id="defaultInline2">
+                <label class="custom-control-label" for="defaultInline2">2</label>
+            </div>
+
+            <!-- Default inline 3-->
+            <div class="custom-control custom-checkbox custom-control-inline">
+                <input type="checkbox" class="custom-control-input" id="defaultInline3">
+                <label class="custom-control-label" for="defaultInline3">3</label>
+            </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
